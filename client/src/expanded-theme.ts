c04dd5d7c731ddbc92681eabee783a8e465f85b0
Yu Fo Hon <117070313+YUFOHON@users.noreply.github.com>
2023-06-23 21:44:36 +0800

    ignore

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,7 +1,7 @@
-// eslint-disable-next-line
-import { PaletteColor } from "@mui/material/styles/createPalette";
+// @ts-ignore:6133
+import { PaletteColor } from '@mui/material/styles/createPalette';
 
-declare module "@mui/material/styles/createPalette" {
+declare module '@mui/material/styles/createPalette' {
   interface PaletteColor {
     [key: number]: string;
   }
